71b9cdfbbb0afb3b80e0d1ae897fa6b0fcb28542
易鑫 <58894205+yixinNB@users.noreply.github.com>
2023-03-16 00:21:32 +0800

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,7 @@ module.exports = {
     'react/no-unescaped-entities': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['off', 'unix'],
     indent: 'off',
     quotes: 'off',
   },
